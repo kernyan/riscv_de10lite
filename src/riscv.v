28044c22bdec8a0169dbaf52a612a78c99a31ae5
kernyan <kernyan@users.noreply.github.com>
2021-08-03 18:23:08 -0400

    minor

--- src/riscv.v
+++ src/riscv.v
@@ -1,6 +1,6 @@
 // cpu
 
-//`define CPU
+`define CPU
 
 module riscv_i (
   input clk,
@@ -19,25 +19,30 @@ Strategy
 5. store
 */
 
-`define MEM_SIZE 2055
+localparam MEMSIZE = 2055;
+localparam ENTRY   = 'h8000_0000;
 
-reg [31:0] mem[0:`MEM_SIZE];
-integer i;
+reg [31:0] mem[0:MEMSIZE];
+reg [31:0] PC;
+
+function [31:0] idx(input [31:0] addr);
+begin
+idx = (addr - ENTRY) >> 2;
+end
+endfunction
 
 initial 
 begin
   $readmemh("tests/rv32ui-p-fence_i.dat", mem); 
-
-  for(i=0; i <= `MEM_SIZE; i = i + 1)
-    $display("Memory [%0d] = %08x", i, mem[i]);
-  
+  PC = ENTRY;
   #1000 $finish;
 end
 
 `ifdef CPU
 always @(posedge clk)
 begin
-  $display("%d", $time);
+  $display("%d %08x %08x", $time, PC, mem[idx(PC)]);
+  PC += 4;
 end
 `endif
 
